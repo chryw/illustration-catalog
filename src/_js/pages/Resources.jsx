152cc0d93d09dc535a5805ad3b0988c417183b27
Cherry Wang <chryw@catenology.com>
2018-02-08 10:58:17 -0800

    Added new illustrations; put template files in zip

--- src/_js/pages/Resources.jsx
+++ src/_js/pages/Resources.jsx
@@ -6,8 +6,8 @@ const Resources = () => (
     <h3>Adobe Illustrator templates</h3>
     <p>Color palette is included in the templates.</p>
     <ul>
-      <li><a href="files/vscom-template.ait" download>Illustration template for vs.com (100 artboards)</a></li>
-      <li><a href="files/vsts-template.ait" download>Illustration template for VSTS (100 artboards)</a></li>
+      <li><a href="files/vscom-template.zip" download>Illustration template for vs.com (100 artboards)</a></li>
+      <li><a href="files/vsts-template.zip" download>Illustration template for VSTS (100 artboards)</a></li>
     </ul>
   </div>
 );
